4b069b3fe312e19e7dad520b72d90714cd00800c
gerard18em <gerard18em@gmail.com>
2014-04-01 16:33:17 +0200

    inici2

---
+++ SQLQueryTesting/src/Start.java
@@ -0,0 +1,13 @@
+
+public class Start {
+
+	public Start() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
